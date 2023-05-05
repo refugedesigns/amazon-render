82c207bdd2ae12a83fd807770c949236a069f731
Erasmus <refugedesigns17@gmail.com>
2023-05-05 22:12:12 +0000

    recompiled for render

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import { Provider } from 'react-redux'
+import Router from "next/router";
+import ProgressBar from "@badrap/bar-of-progress";
 import { Provider as NextAuthProvider } from "next-auth/client"
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from 'redux-persist';
@@ -6,6 +8,17 @@ import store from "../slices/store"
 import '../styles/globals.css'
 
 let persistor = persistStore(store)
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#DC143C",
+  className: "",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
